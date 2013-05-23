1897f347cd9ad7b864c625eeaac22be9977fd412
Aki Mimoto <aki+launchpad@zaber.com>
2013-05-23 15:42:18 -0700

    [IMP] Remove overly aggressive restrictions on stock move edits

--- stock/stock.py
+++ stock/stock.py
@@ -1698,13 +1698,6 @@ class stock_move(osv.osv):
     def write(self, cr, uid, ids, vals, context=None):
         if isinstance(ids, (int, long)):
             ids = [ids]
-        if uid != 1:
-            frozen_fields = set(['product_qty', 'product_uom', 'product_uos_qty', 'product_uos', 'location_id', 'location_dest_id', 'product_id'])
-            for move in self.browse(cr, uid, ids, context=context):
-                if move.state == 'done':
-                    if frozen_fields.intersection(vals):
-                        raise osv.except_osv(_('Operation forbidden'),
-                                             _('Quantities, UoMs, Products and Locations cannot be modified on stock moves that have already been processed (except by the Administrator)'))
         return  super(stock_move, self).write(cr, uid, ids, vals, context=context)
 
     def copy(self, cr, uid, id, default=None, context=None):
